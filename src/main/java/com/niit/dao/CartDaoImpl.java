89467963b267e7a0bb817d36eb6561b87553687c
kuhuroy <38374615+kuhuroy@users.noreply.github.com>
2018-07-13 02:27:25 -0700

    Add files via upload

---
+++ src/main/java/com/niit/dao/CartDaoImpl.java
@@ -0,0 +1,21 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.niit.model.Cart;
+@Repository
+public class CartDaoImpl implements CartDao{
+
+	public void addToCart(Cart cart) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Cart> getCartItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
